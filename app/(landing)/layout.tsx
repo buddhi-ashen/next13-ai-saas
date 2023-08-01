6bfd25e6b8e61d6f3fe372a0361c40dbfedac451
Buddhi Ashen <buddhiict@gmail.com>
2023-08-01 07:21:00 +0530

    Update layout.tsx

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -4,7 +4,14 @@ const LandingLayout = ({
   children: React.ReactNode;
 }) => {
   return (
-    <main className="h-full bg-[#111827] overflow-auto">
+    <main className="h-full overflow-auto " style={{
+      backgroundImage: 'url("https://source.unsplash.com/random/?dark")',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      objectFit: 'cover',
+       // Darken the background using an rgba color
+    }}>
       <div className="mx-auto max-w-screen-xl h-full w-full">
         {children}
       </div>
